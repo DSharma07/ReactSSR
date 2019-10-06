341af2a3a562e6940de9a404ec441e0202346e0c
Dheeraj Sharma <de.jsharma@gmail.com>
2019-10-06 10:17:33 +0100

    added redux and routing

--- server/src/client/client.js
+++ server/src/client/client.js
@@ -1,5 +1,20 @@
+import 'babel-polyfill';
 import React from 'react';
 import ReactDom from 'react-dom';
-import Home from './components/Home'; // ES6
+import  { BrowserRouter } from 'react-router-dom';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';   // handle async operations
+import { Provider } from 'react-redux'; // connect store to client
+import Routes from './Routes';
+import reducers from './reducers';
 
-ReactDom.hydrate(<Home />, document.querySelector('#root'));
+const store = createStore(reducers, {}, applyMiddleware(thunk));
+
+ReactDom.hydrate(
+    <Provider store={ store }>
+        <BrowserRouter>
+            <Routes />
+        </BrowserRouter>
+    </Provider>
+    , document.querySelector('#root')
+);

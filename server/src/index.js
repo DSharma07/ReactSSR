46f3e998a6e5f062a06f99f8bc396deb7ce3fb7f
Dheeraj Sharma <de.jsharma@gmail.com>
2019-10-08 15:53:15 +0100

    code refactoring for server side data fetching

--- server/src/index.js
+++ server/src/index.js
@@ -1,15 +1,31 @@
 import 'babel-polyfill';
 import express from 'express';
+import  { matchRoutes } from 'react-router-config';
+import proxy from 'express-http-proxy';
+import Routes from './client/Routes';
 import renderer from './helpers/renderer.js';
 import createStore from  './helpers/createStore';
 
 const app = express();
 
+app.use('/api', proxy('http://react-ssr-api.herokuapp.com', {
+    proxyReqOptDecorator(opts) {
+        opts.headers['x-forward-host'] = 'localhost:3000';
+        return opts;
+    }
+ })
+);
 app.use(express.static('public'));
 app.get('*', (req, res) => {
-    const store = createStore();
+    const store = createStore(req);
 
-    res.send(renderer(req, store));
+    const promises = matchRoutes(Routes, req.path).map(({route}) => {
+        route.loadData ? route.loadData(store) : null;
+    });
+
+    Promise.all(promises).then(() => {
+        res.send(renderer(req, store));
+    });
 });
 
 app.listen(3000, () => {
